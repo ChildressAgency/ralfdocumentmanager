b7779749d9a70fc42d84517f96c95d1e42350b0b
childressdev <jcampbell@childressagency.com>
2019-01-02 17:30:26 -0500

    updated with new actions

--- templates/ralfdocs-view-report-loop.php
+++ templates/ralfdocs-view-report-loop.php
@@ -10,7 +10,6 @@ $report_article_id = get_the_ID(); ?>
       <a href="#article_id-<?php echo $report_article_id ?>" class="meta-btn report-expand hidden-print collapsed" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="article_id-<?php echo $report_article_id; ?>" class="collapsed"></a>
       <span class="article-handle"></span>
       <?php do_action('ralfdocs_article_meta', $report_article_id); ?>
-      <?php //usaidralf_show_article_meta(get_post_type(), $report_article_id); ?>
     </div>
   </header>
 
@@ -21,7 +20,7 @@ $report_article_id = get_the_ID(); ?>
       </div>
       <?php if(get_field('conditions')): ?>
         <div class="activity-conditions">
-          <h2><?php _e('CONDITIONS', 'ralfreports'); ?></h2>
+          <h2><?php echo esc_html__('CONDITIONS', 'ralfdocs'); ?></h2>
           <?php the_field('conditions'); ?>
         </div>
       <?php endif; ?>
@@ -30,10 +29,10 @@ $report_article_id = get_the_ID(); ?>
     <?php
       $impact_ids = get_field('related_impacts', false, false);
       if(!empty($impact_ids)):
-        $impacts_by_sector = usaidralf_get_impacts_by_sector($impact_ids); ?>
+        $impacts_by_sector = ralfdocs_get_impacts_by_sector($impact_ids); ?>
 
         <section class="impact-by-sector">
-          <h2><?php _e('IMPACT BY SECTOR', 'usaidralf'); ?><span class="dashicons dashicons-excerpt-view" data-toggle="tooltip" data-position="top" title="<?php _e('Expand All', 'usaidralf'); ?>"></span></h2>
+          <h2><?php echo esc_html__('IMPACT BY SECTOR', 'ralfdocs'); ?><span class="dashicons dashicons-excerpt-view" data-toggle="tooltip" data-position="top" title="<?php echo esc_attr__('Expand All', 'ralfdocs'); ?>"></span></h2>
           <div class="panel-group" id="impacts-accordion" role="tablist" aria-multiselectable="true">
             <?php 
                 $i = 0;
@@ -44,23 +43,23 @@ $report_article_id = get_the_ID(); ?>
                   <div class="panel panel-default">
                     <div class="panel-heading" role="tab" id="impact-title<?php echo $i; ?>">
                       <h3 class="panel-title">
-                        <a href="<?php echo get_permalink($impact->impact_id); ?>" class="sector-popout hidden-print" target="_blank">
-                          <?php echo $impact->impact_title; ?>
-                          <span class="dashicons dashicons-external" data-toggle="tooltip" data-position="top" title="<?php echo $sector['sector_name']; ?>"></span>
+                        <a href="<?php echo esc_url(get_permalink($impact->impact_id)); ?>" class="sector-popout hidden-print" target="_blank">
+                          <?php echo esc_html($impact->impact_title); ?>
+                          <span class="dashicons dashicons-external" data-toggle="tooltip" data-position="top" title="<?php echo esc_attr__($sector['sector_name']); ?>"></span>
                         </a>
                       </h3>
                       <div class="impact-by-sector-meta">
                         <a href="#impact<?php echo $report_article_id . '-' . $i; ?>" class="meta-btn report-expand hidden-print collapsed" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="impact<?php echo $i; ?>"></a>
                         <?php
                           //get all sectors for this impact to use for meta btns
-                          usaidralf_show_article_meta('impacts', $impact->impact_id); 
+                          do_action('ralfdocs_article_meta', $impact->impact_id);
                         ?>
                       </div>
                     </div>
                     <div class="clearfix"></div>
                     <div id="impact<?php echo $report_article_id . '-' . $i; ?>" class="panel-collapse collapse print-visible" role="tabpanel" aria-labelledby="impact-title<?php echo $i; ?>">
                       <div class="panel-body">
-                        <?php echo $impact->impact_description; ?>
+                        <?php echo wp_kses_post($impact->impact_description); ?>
                       </div>
                     </div>
                   </div>
