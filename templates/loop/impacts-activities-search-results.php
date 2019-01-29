5a1f1d27d09750b74c91f814d6e67abceb857ff4
childressdev <jcampbell@childressagency.com>
2019-01-29 15:56:56 -0500

    added missing article_id var

--- templates/loop/impacts-activities-search-results.php
+++ templates/loop/impacts-activities-search-results.php
@@ -12,15 +12,14 @@ include ralfdocs_get_template('loop/impacts-activities-tab.php');
   <div id="impacts-activities">
 
     <?php 
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
       if(!empty($impacts_activities->posts)){
         foreach($impacts_activities->posts as $post){
           setup_postdata($post);
+          $article_id = $post->ID;
           include ralfdocs_get_template('loop/loop-item.php');
         }
         wp_reset_postdata();
-        ralfdocs_pagination();
+        ralfdocs_pagination($impacts_activities);
       }
       else{
         include ralfdocs_get_template('loop/no-results.php');
