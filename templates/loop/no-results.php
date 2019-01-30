9804e81f73a7a2de0f7e473f82f39e73187dcf2b
childressdev <jcampbell@childressagency.com>
2019-01-30 10:48:02 -0500

    added override comment

--- templates/loop/no-results.php
+++ templates/loop/no-results.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Template for displaying no results
+ * 
+ * Can be overridden with custom template file here:
+ * THEME_STYLESHEET_DIRECTORY/ralfdocs-templates/loop/no-results.php
  */
 if(!defined('ABSPATH')){ exit; }
 ?>
