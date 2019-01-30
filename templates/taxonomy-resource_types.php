c4525e64a955268733d7e0a4f20bcbeb0525eb1f
childressdev <jcampbell@childressagency.com>
2019-01-30 11:32:07 -0500

    removed extraneous loop file

--- templates/taxonomy-resource_types.php
+++ templates/taxonomy-resource_types.php
@@ -34,7 +34,18 @@ get_header(); ?>
               )
             ));
 
-            include ralfdocs_get_template('loop/resource-type-loop.php');
+            if(!empty($resources->posts)){
+              foreach($resources->posts as $post){
+                setup_postdata($post);
+                $article_id = $post->ID;
+                include ralfdocs_get_template('loop/loop-item.php');
+              }
+              wp_reset_postdata();
+              ralfdocs_pagination($resources);
+            }
+            else{
+              include ralfdocs_get_template('loop/no-results.php');
+            }
           ?>
 
         </main>
