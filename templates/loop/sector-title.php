184271e43bc77b56d6518815c5b8abd901c53040
childressdev <jcampbell@childressagency.com>
2019-01-30 10:48:48 -0500

    added

---
+++ templates/loop/sector-title.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template for showing sector title on taxonomy archive page
+ * 
+ * Can be overridden with custom template file here:
+ * THEME_STYLESHEET_DIRECTORY/loop/sector-title.php
+ */
+if(!defined('ABSPATH')){ exit; }
+?>
+
+<h1 class="sector-title">
+  <?php 
+    $sector_icon = get_field('sector_icon', 'sectors_' . $current_sector->term_id);
+    if($sector_icon): ?>
+      <img src="<?php echo esc_url($sector_icon); ?>" class="img-circle img-responsive" alt="<?php echo esc_attr($current_sector->name) . ' ' . esc_attr__('Sector', 'ralfdocs'); ?>" style="background-color:<?php the_field('sector_color', 'sectors_' . $current_sector->term_id); ?>" />
+  <?php endif; ?>
+  <?php echo esc_html($current_sector->name); ?>
+</h1>
