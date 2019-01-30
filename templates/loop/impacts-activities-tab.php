6742a3bcb28b81409011b968bca6a0702e5e1ee0
childressdev <jcampbell@childressagency.com>
2019-01-30 10:47:41 -0500

    added override comment

--- templates/loop/impacts-activities-tab.php
+++ templates/loop/impacts-activities-tab.php
@@ -3,6 +3,9 @@
  * Tabs for search results loops and taxonomy loop pages
  * This displays the Impacts / Activities tab as active
  * 
+ * Can be overridden with custom template file here:
+ * THEME_STYLESHEET_DIRECTORY/ralfdocs-templates/loop/impacts-activities-tab.php
+ * 
  */
 if(!defined('ABSPATH')){ exit; }
 ?>
