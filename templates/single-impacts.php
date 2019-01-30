1ea0aa83c04b060919c4243c719f66ed10c7579d
childressdev <jcampbell@childressagency.com>
2019-01-30 15:45:24 -0500

    restructured

--- templates/single-impacts.php
+++ templates/single-impacts.php
@@ -1,4 +1,11 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Template for viewing single Impacts
+ * 
+ * Can be overridden with custom template file here:
+ * THEME_STYLESHEET_DIRECTORY/ralfdocs-templates/single-impacts.php
+ */
+get_header(); ?>
   <div class="page-content">
     <div class="container">
       <div class="row">
@@ -13,7 +20,33 @@
               if(have_posts()){
                 while(have_posts()){
                   the_post();
-                  do_action('ralfdocs_impacts_loop');
+                  //do_action('ralfdocs_impacts_loop');
+                  $impact_id = get_the_ID();
+                  $sectors = get_the_terms($impact_id, 'sectors'); ?>
+
+                  <article class="ralf-article">
+                    <header class="result-header">
+                      <h1><?php the_title(); ?></h1>
+                      <div class="result-meta">
+                        <?php 
+                          $article_id = $impact_id;
+                          include ralfdocs_get_template('loop/article-meta.php'); 
+                        ?>
+                      </div>
+                    </header>
+
+                    <section class="result-content">
+                      <?php the_content(); ?>
+                    </section>
+                    <?php
+                      echo do_shortcode('[report_button]');
+
+                      do_action('ralfdocs_related_activities', $impact_id, 'impacts');
+
+                      do_action('ralfdocs_related_resources', $impact_id);
+                    ?>
+                  </article>
+            <?php
                 }
               }
             ?>
--- templates/single-resources.php
+++ templates/single-resources.php
@@ -1,4 +1,11 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Template for showing single Resources page
+ * 
+ * Can be overridden with custom template file here:
+ * THEME_STYLESHEET_DIRECTORY/ralfdocs-templates/single-resources.php
+ */
+get_header(); ?>
 <div class="page-content">
   <div class="container">
     <div class="row">
@@ -8,11 +15,38 @@
       <div class="col-sm-8 col-md-9">
         <main class="result">
           <?php do_action('ralfdocs_back_button'); ?>
+
           <?php 
             if(have_posts()){
               while(have_posts()){
                 the_post();
-                do_action('ralfdocs_resources_loop');
+                //do_action('ralfdocs_resources_loop');
+                $resource_id = get_the_ID(); ?>
+
+                <article class="ralf-article">
+                  <header class="result-header">
+                    <h1><?php the_title(); ?></h1>
+                    <div class="result-meta">
+                      <?php
+                        $article_id = $resource_id;
+                        include ralfdocs_get_template('loop/article_meta');
+                      ?>
+                    </div>
+                  </header>
+
+                  <section class="result-content">
+                    <?php the_content(); ?>
+                  </section>
+                  <?php 
+                    echo do_shortcode('[report_button]');
+
+                    do_action('ralfdocs_related_activities', $resource_id, 'resources');
+
+                    do_action('ralfdocs_resources_related_impacts', $resource_id);
+                  ?>
+                </article>
+
+          <?php
               }
             }
           ?>
