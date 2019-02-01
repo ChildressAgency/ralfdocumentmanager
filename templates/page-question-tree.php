e0772e6b2cb15c137390eeaadeabcd81354b580e
childressdev <jcampbell@childressagency.com>
2019-02-01 15:15:36 -0500

    added intro

--- templates/page-question-tree.php
+++ templates/page-question-tree.php
@@ -6,14 +6,24 @@
  * THEME_STYLESHEET_DIRECTORY/ralfdocs-templates/page-question-tree.php
  */
 
-get_header('question_tree'); ?>
-<div class="full-page" style="background-image:url(images/);">
-  <div class="full-page-inner">
-    <div class="content-container">
-      <article class="content-inner">
+get_header();
 
+$page_id = get_the_ID();
+$bg_img_id = get_post_meta($page_id, 'background_image', true);
+$bg_img = wp_get_attachment_image_src($bg_img_id, 'full');
+$bg_img_url = $bg_img[0];
+?>
+
+<div id="question-tree" style="background-image:url(<?php echo esc_url($bg_img_url); ?>); <?php echo esc_html(get_field('background_image_css')); ?>">
+  <div class="container">
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      <article>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+        <a href="#" id="qt-start" class="btn-main">Start<i class="glyphicon glyphicon-refresh no-show"></i></a>
       </article>
-    </div>
+    <?php endwhile; endif; ?>
   </div>
+  <div class="full-page-overlay"></div>
 </div>
-<?php get_footer('question_tree');
+<?php get_footer();
